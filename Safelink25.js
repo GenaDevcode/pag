8934e89e197064395fb9abc9ebed355cd4379713
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-02-24 00:00:21 +0100

    fucio
    hd

---
+++ Safelink25.js
@@ -0,0 +1,19 @@
+function Login(){
+
+    var done=0; 
+    var usuario=document.login.usuario.value; 
+    var password=document.login.password.value; 
+    
+    
+     if (usuario=="Genfans" && password=="user1547") { 
+    window.location="https://genfans.blogspot.com/2021/09/blog-post.html"; 
+    } else if
+    (usuario=="Genfans895" && password=="Genfans78yt") { 
+    window.location="https://genfans.blogspot.com/2021/09/blog-post.html"; 
+    } 
+      
+      else {
+          alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
+      }
+      
+    }
