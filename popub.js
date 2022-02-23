094b861ce2974190a72902b6ece4c30e9ddcb69a
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-02-24 00:18:56 +0100

    popub
    poub

---
+++ popub.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+ window.open("https://www.youtube.com/embed/Fu-sBIXMXmA?autoplay=1","PopUp","width=ANCHO, height=ALTO, scrollbars=yes, menubar=no, status=no, location=no, resizable=yes");
+}
