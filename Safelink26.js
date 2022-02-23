32a9ba76ad39cad99de0c42d5b0cf2c917facfc4
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-02-24 00:06:16 +0100

    login
    login

---
+++ Safelink26.js
@@ -0,0 +1,19 @@
+function Login(){
+
+    var done=0; 
+    var usuario=document.login.usuario.value; 
+    var password=document.login.password.value; 
+    
+    
+     if (usuario=="Onlyfans" && password=="user1") { 
+    window.location="https://freeonlyfansde.blogspot.com/2021/09/log.html"; 
+    } else if
+    (usuario=="Free" && password=="12345") { 
+    window.location="https://freeonlyfansde.blogspot.com/2021/09/log.html"; 
+    } 
+      
+      else {
+          alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
+      }
+      
+    }
