6a1b6b9d5d7c5b256796c5b32df7e93197657203
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-02-24 22:40:04 +0100

    df
    sdf

---
+++ anunciosasdew.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '7d8a4f9345869139c00c376f2648197b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/7d8a4f9345869139c00c376f2648197b/invoke.js"></scr' + 'ipt>');
