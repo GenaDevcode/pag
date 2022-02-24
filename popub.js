474a085968af75d541d49962250d776a2a045f06
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-02-24 22:03:45 +0100

    Update popub.js

--- popub.js
+++ popub.js
@@ -1,3 +1,7 @@
 window.onload = function() {
  window.open("https://www.youtube.com/embed/Fu-sBIXMXmA?autoplay=1","PopUp","width=ANCHO, height=ALTO, scrollbars=yes, menubar=no, status=no, location=no, resizable=yes");
 }
+
+ var adfly_id = 16033609;
+    var popunder_frequency_delay = 0;
+    var adfly_google_compliant = false;
