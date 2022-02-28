1c39bd42086b1c16a3b6c875eae969480483a421
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-02-28 03:04:16 +0100

    Update acortagenfans.js

--- acortagenfans.js
+++ acortagenfans.js
@@ -1,4 +1,4 @@
- onclick="openad()"
+
   	function openad(){
 		window.open('https://www.appcreator24.com/open1763776/a');
 		//window.open('https://goraps.com/fullpage.php?section=General&pub=876342&ga=g');
