8603e9c523a903aa082e43c7f337f8d2bf88d4ba
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-03-01 03:12:40 +0100

    Update Safelink26.js

--- Safelink26.js
+++ Safelink26.js
@@ -1,19 +1,16 @@
-function Login(){
+function Login() {
 
     var done=0; 
     var usuario=document.login.usuario.value; 
     var password=document.login.password.value; 
-    
-    
-     if (usuario=="Onlyfans" && password=="user1") { 
-    window.location="https://freeonlyfansde.blogspot.com/2021/09/log.html"; 
-    } else if
-    (usuario=="Free" && password=="12345") { 
-    window.location="https://freeonlyfansde.blogspot.com/2021/09/log.html"; 
-    } 
-      
-      else {
-          alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
-      }
-      
-    }
+    var a = prompt("Haga clic en cualquier anuncio y luego copie la URL del anuncio y péguelo aquí.");
+  
+   if (a.includes("https://" || usuario=="Onlyfans" && password=="user1")) {
+   window.location="https://freeonlyfansde.blogspot.com/2021/09/log.html";
+  } else if (a.includes("https://"|| usuario=="Free" && password=="12345")) {
+  window.location="https://freeonlyfansde.blogspot.com/2021/09/log.html"; 
+  }
+     else {
+alert("Porfavor ingrese, nombre de usuario y contraseña correctos y el link de un anuncio.");
+  }
+}
