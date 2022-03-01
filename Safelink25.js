bf51cab4607d22de26e79458bf7fe9644de7f92f
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-03-01 06:16:24 +0100

    Update Safelink25.js

--- Safelink25.js
+++ Safelink25.js
@@ -5,7 +5,7 @@ function Login() {
     var password=document.login.password.value; 
     var a = prompt("Haga clic en cualquier anuncio y luego copie la URL del anuncio y péguelo aquí.");
   
-   if (a.includes("https://genfans.blogspot.com" || usuario=="Genfans" && password=="user1")) {
+   if (a.includes("https://" || usuario=="Genfans" && password=="user1")) {
    window.location="https://genfans.blogspot.com/2021/09/blog-post.html";
   } else if (a.includes("https://" || usuario=="GenfansFree" && password=="12345")) {
   window.location="https://genfans.blogspot.com/2021/09/blog-post.html"; 
