ffc2b8ae4be1943e43d24348dd0aad6017647580
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-03-02 01:57:49 +0100

    API1AD
    API1AD

---
+++ API1AD.js
@@ -0,0 +1,11 @@
+document.getElementById('aLink').addEventListener('click',function(e){
+  e.preventDefault();
+  var a = prompt("Haga clic en cualquier anunci0 y luego copie la URL del anunci0 y péguelo aquí.");
+  //GenDevCode
+   if (a.includes("https://")) {
+   window.location = this.href
+  } else {
+   alert("Ingresaste un enlace equivocado");
+  }
+  
+});
