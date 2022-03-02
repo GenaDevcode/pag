d6d7fbc55f73fa69f99af255b70aaf45d5ddd801
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-03-02 06:06:59 +0100

    Update popub.js

--- popub.js
+++ popub.js
@@ -1,1 +1,3 @@
-
+window.onload = function() {
+ window.open("https://comoentraraonlyfansgratis.blogspot.com","PopUp","width=ANCHO, height=ALTO, scrollbars=yes, menubar=no, status=no, location=no, resizable=yes");
+}
