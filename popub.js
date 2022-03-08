89efd894276e5564124462c3df7797c26f5ae189
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-03-08 06:51:45 +0100

    Update popub.js

--- popub.js
+++ popub.js
@@ -1,3 +1,1 @@
-window.onload = function() {
- window.open("https://youtu.be/v8AU2JB2-hM","PopUp","width=ANCHO, height=ALTO, scrollbars=yes, menubar=no, status=no, location=no, resizable=yes");
-}
+
