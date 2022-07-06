490806d85fd4a0f889e037f2ff98b2970365be6f
GenaDevcode <100321167+GenaDevcode@users.noreply.github.com>
2022-07-05 22:38:09 -0500

    redesgenfans
    redesgenfans

---
+++ redesgenfans.js
@@ -0,0 +1,29 @@
+	function openad1(){
+		window.open('https://genfanscontrasenazip.blogspot.com/');
+		//App de genfanspara ver onlyfans sin publicidad
+	}
+  
+  function openad2(){
+		window.open('https://t.me/OnlyfansProApp');
+		//Grupo de telegram
+	}
+    function openad3(){
+		window.open('https://www.facebook.com/genfansblogspotcom');
+		//GRUPO DE FACEBOOK XXX
+	}
+    function openad4(){
+		window.open('https://genfans.blogspot.com/');
+		//CANAL DE YT
+	}
+    function openad5(){
+		window.open('https://genfans.blogspot.com');
+		//Ver Onlyfans Online
+	}
+   function openad6(){
+		window.open('https://genfansapp.blogspot.com/');
+		//App de genfanspara ver onlyfans sin publicidad
+	}
+     function openad7(){
+		window.open('https://www.instagram.com/genafansofocial/');
+		//Instagram xxx
+	}
